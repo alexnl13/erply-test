7d8d08a6b878460d9d49c46220c8cd2a0e9cc7eb
Alex Neil <box4pm@gmail.com>
2019-02-23 10:58:17 +0200

    Updates

---
+++ precache-manifest.8ac20acf32dc53a646f0d6304120f03a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20db3b6bbc786285e57a",
+    "url": "/erply-test/static/js/runtime~main.20db3b6b.js"
+  },
+  {
+    "revision": "68ddc9525c7f3243ffe3",
+    "url": "/erply-test/static/js/main.68ddc952.chunk.js"
+  },
+  {
+    "revision": "23ba732b6b463f1ff3e5",
+    "url": "/erply-test/static/js/2.23ba732b.chunk.js"
+  },
+  {
+    "revision": "68ddc9525c7f3243ffe3",
+    "url": "/erply-test/static/css/main.14473b02.chunk.css"
+  },
+  {
+    "revision": "d5ed55aedbf410613c3128d90cb1cc97",
+    "url": "/erply-test/index.html"
+  }
+];
